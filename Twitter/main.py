cf6268990c8c314a61ef70d998807cfa51e0e7c0
AlessandroBregoli <alessandroxciv@gmail.com>
2017-06-07 14:07:31 +0200

    Inizio lavoro su twitter API

---
+++ Twitter/main.py
@@ -0,0 +1,9 @@
+import twitter
+consumer_key="yh10KkaZmuWZ1GdXVP1SvqiyK"
+consumer_secret="WB6qemh6UW4uPgdOKpOaN27ZyQfTXc9L9VFwH74lDuydCExi1R"
+access_token_key="2387291089-KrXUNlvBaKfdYGz8PpcS4EKuIbLtHfs4w0uuH81"
+access_token_secret="fhaZSX3QFbGdth993aXPW1iQkgSOz5GJH6kJpcqERFDgC"
+api = twitter.Api(consumer_key=consumer_key,\
+		 consumer_secret=consumer_secret,\
+		 access_token_key=access_token_key,\
+		 access_token_secret=access_token_secret) 

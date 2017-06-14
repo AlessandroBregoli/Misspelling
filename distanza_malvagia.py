bca356654200a10cbda2a0c5c4884447a5329c1e
Pietro Brenna <pietrobrenna@hotmail.it>
2017-06-14 10:40:32 +0200

    aggiunto try catch a caso

--- distanza_malvagia.py
+++ distanza_malvagia.py
@@ -7,10 +7,8 @@ def distanza_malvagia(s1, s2, m_err):
     mat[0][0] = m_err.azzecca
     for i in range(len(s1)+1):
         mat[0][i] = m_err.p_inserzione ** i
-        print("1")
     for j in range(len(s2)+1):
         mat[j][0] = m_err.p_omissione ** j
-        print("2")
     for j in range(1,len(s2)+1):
         for i in range(1, len(s1)+1):
             if s1[i-1] == s2[j-1]:
@@ -18,7 +16,11 @@ def distanza_malvagia(s1, s2, m_err):
             else:
                 inserz = mat[j-1][i] * m_err.p_inserzione
                 omiss = mat[j][i-1] * m_err.p_omissione
-                sostit = mat[j-1][i-1] * m_err.probs[s2[j-1]][s1[i-1]]
+                try: 
+                    prob_sost = m_err.probs[s2[j-1]][s1[i-1]]
+                except KeyError:
+                    prob_sost = 0
+                sostit = mat[j-1][i-1] * prob_sost
                 mat[j][i] = max(inserz, omiss, sostit)
     return mat[len(s2),len(s1)]
 import adiacenti

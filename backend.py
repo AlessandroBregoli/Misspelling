4852a3cdbab891767274643c2795761cdc273d3f
alessandrobregoli <alessandroxciv@gmailc.om>
2017-06-27 11:47:26 +0200

    Aggiunta la possibilità di scegliere quale modello di errore usare

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from flask import Flask,request, g
 import json
 from hyperviterbi import Hyperviterbi
 from freq_analizer import PriorGenerator
+from adiacenti import ModelloErrore
 
 app = Flask(__name__)
 priorGenerator = None 
@@ -17,13 +18,18 @@ def get_prior_generator():
 def get_stop_symbols():
      return json.dumps(get_prior_generator().stopSymbols)
 
-@app.route('/viterbi', methods=['POST'])
-def viterbi():
+@app.route('/viterbi/<input_type>', methods=['POST'])
+def viterbi(input_type):
     js = request.get_json(force=True)
     pre_analized = js['mat']
     phrase = js['phrase']
     priorGenerator = get_prior_generator()
     v = Hyperviterbi(priorGenerator, 10)
+    if input_type == "tastiera":
+        v.m_err.calcola_adiacenze()
+    elif input_type == "braille":
+        v.m_err.calcola_adiacenze("adiacentiBraille.txt")
+        
     corrected_phrase = v.viterbi(phrase, pre_analized)
     ret_dic = {"mat":pre_analized,"viterbi":corrected_phrase}
     return json.dumps(ret_dic)

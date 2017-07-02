5593ce98f067bb87452f492b49d345f1a827cd4e
alessandrobregoli <alessandroxciv@gmailc.om>
2017-07-02 17:54:18 +0200

    Correzione main viterbi

--- main_viterbi.py
+++ main_viterbi.py
@@ -5,9 +5,9 @@ import os
 
 p = freq_analizer.PriorGenerator(1e-20)
 p.deserialize("freq_analized.json")
+p.load_stop_symbols_from_file("stop_symbols.txt")
 
-
-v = hyperviterbi.Hyperviterbi(p, 25)
+v = hyperviterbi.Hyperviterbi(p, 10)
 
 while True:
     print("> ",end="")
